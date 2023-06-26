d936eab40bd953ac7426c2485cff75bb172e8a7c
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2023-06-26 14:30:22 -0300

    Inseri o HTML/CSS dos casos de uso Realizar Login, Realizar Cadastro e Enviar Mensagem

---
+++ source/WebAppPratelivros/Controllers/LeitorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebAppPratelivros.Controllers
+{
+    public class LeitorController : Controller
+    {
+        // GET: Leitor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
---
+++ source/WebAppPratelivros/Models/Leitor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppPratelivros.Models
+{
+    public class Leitor
+    {
+        public string nome;
+        public string username;
+        public string email;
+        public string senha;
+        public string celular;
+        public string idade;
+        public string genero;
+    }
+}
