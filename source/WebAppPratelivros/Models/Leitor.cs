762e6de8d1059d1df256a32b2e62046f194ecf73
monzadrifteiro <nunes.lima@escolar.ifrn.edu.br>
2023-09-11 17:47:56 -0300

    Continuação dos tratamentos de exceções

--- source/WebAppPratelivros/Models/Leitor.cs
+++ source/WebAppPratelivros/Models/Leitor.cs
@@ -9,20 +9,24 @@ namespace WebAppPratelivros.Models
 {
     public class Leitor
     {
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Nome' é obrigatório.")]
+        [MaxLength(30, ErrorMessage = "O campo 'Nome' deve ser composto por, no máximo, 30 caracteres")]
         public string Nome { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Username' é obrigatório.")]
+        [MaxLength(15, ErrorMessage = "O campo 'Username' deve ser composto por, no máximo, 15 caracteres")]
         public string Username { get; set; }
         [Key]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'E-mail' é obrigatório.")]
+        [MaxLength(30, ErrorMessage = "O campo 'E-mail' deve ser composto por, no máximo, 30 caracteres")]
         public string Email { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Senha' é obrigatório.")]
+        [MaxLength(20, ErrorMessage = "O campo 'Senha' deve ser composto por, no máximo, 20 caracteres")]
         public string Senha { get; set; }
         public string Celular { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Idade' é obrigatório.")]
         public string Idade { get; set; }
-        [Required]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Gênero' é obrigatório.")]
         public string Genero { get; set; }
-        [Required]
         public float Avaliacao { get; set; }
     }
 }
--- source/WebAppPratelivros/Models/Livro.cs
+++ source/WebAppPratelivros/Models/Livro.cs
@@ -10,28 +10,34 @@ namespace WebAppPratelivros.Models
     public class Livro
     {
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Título' é obrigatório.")]
+        [MaxLength(50, ErrorMessage = "O campo 'Título' deve ser composto por, no máximo, 50 caracteres")]
         public string Titulo { get; set; }
         [Key]
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'IBSN' é obrigatório.")]
-        [MaxLength(13, ErrorMessage = "O campo 'ISBN' deve ter 13 caracteres")]
-        [MinLength(13, ErrorMessage = "O campo 'ISBN' deve ter 13 caracteres")]
+        [MaxLength(13, ErrorMessage = "O campo 'ISBN' deve ser composto por 13 caracteres")]
+        [MinLength(13, ErrorMessage = "O campo 'ISBN' deve ser composto por 13 caracteres")]
         public string ISBN { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Editora' é obrigatório.")]
+        [MaxLength(30, ErrorMessage = "O campo 'Editora' deve ser composto por, no máximo, 30 caracteres")]
         public string Editora { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Edição' é obrigatório.")]
         public int Edicao { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Idioma' é obrigatório.")]
+        [MaxLength(15, ErrorMessage = "O campo 'Idioma' deve ser composto por, no máximo, 15 caracteres")]
         public string Idioma { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'N° de Páginas' é obrigatório.")]
         public int NPaginas { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Autor' é obrigatório.")]
+        [MaxLength(30, ErrorMessage = "O campo 'Autor' deve ser composto por, no máximo, 30 caracteres")]
         public string Autor { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Gênero' é obrigatório.")]
+        [MaxLength(30, ErrorMessage = "O campo 'Gênero' deve ser composto por, no máximo, 30 caracteres")]
         public string Genero { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Data de Publicação' é obrigatório.")]
         public string DataPub { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Descrição' é obrigatório.")]
         [MinLength(30, ErrorMessage = "O campo 'Descrição' deve ser composto por, no mínimo, 30 caracteres")]
+        [MaxLength(1000, ErrorMessage = "O campo 'Descrição' deve ser composto por, no máximo, 1000 caracteres")]
         public string Descricao { get; set; }
         public System.Guid Id { get; set; }
         [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Capa' é obrigatório.")]
