daeeefd9733988f663305deb8e415bf566920ada
monzadrifteiro <lucas363nunes@gmail.com>
2023-09-18 00:47:02 -0300

    Correções no CDU Reportar Erro.

--- source/WebAppPratelivros/Models/Leitor.cs
+++ source/WebAppPratelivros/Models/Leitor.cs
@@ -9,28 +9,28 @@ namespace WebAppPratelivros.Models
 {
     public class Leitor
     {
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Nome' é obrigatório.")]
-        [MaxLength(30, ErrorMessage = "O campo 'Nome' deve ser composto por, no máximo, 30 caracteres")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Nome' é obrigatório.")]
+        //[MaxLength(30, ErrorMessage = "O campo 'Nome' deve ser composto por, no máximo, 30 caracteres")]
         public string Nome { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Username' é obrigatório.")]
-        [MaxLength(15, ErrorMessage = "O campo 'Username' deve ser composto por, no máximo, 15 caracteres")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Username' é obrigatório.")]
+        //[MaxLength(15, ErrorMessage = "O campo 'Username' deve ser composto por, no máximo, 15 caracteres")]
         public string Username { get; set; }
         [Key]
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'E-mail' é obrigatório.")]
-        [MaxLength(30, ErrorMessage = "O campo 'E-mail' deve ser composto por, no máximo, 30 caracteres")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'E-mail' é obrigatório.")]
+        //[MaxLength(30, ErrorMessage = "O campo 'E-mail' deve ser composto por, no máximo, 30 caracteres")]
         public string Email { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Senha' é obrigatório.")]
-        [MaxLength(20, ErrorMessage = "O campo 'Senha' deve ser composto por, no máximo, 20 caracteres")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Senha' é obrigatório.")]
+        //[MaxLength(20, ErrorMessage = "O campo 'Senha' deve ser composto por, no máximo, 20 caracteres")]
         public string Senha { get; set; }
         public string Celular { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Idade' é obrigatório.")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Idade' é obrigatório.")]
         public string Idade { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Gênero' é obrigatório.")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Gênero' é obrigatório.")]
         public string Genero { get; set; }
         public float Avaliacao { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Tipo' é obrigatório.")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Tipo' é obrigatório.")]
         public Denuncia Tipo { get; set; }
-        [Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Mensagem' é obrigatório.")]
+        //[Required(AllowEmptyStrings = false, ErrorMessage = "O campo 'Mensagem' é obrigatório.")]
         public string Mensagem { get; set; }
     }
 }
